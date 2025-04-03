b4bf18388157aa2d2adb66da9a61ee5033e387c1
Stefan Fries <stefan.fries.burgdorf@gmx.de>
2025-04-03 23:10:06 +0200

    changed comments

--- app/main.py
+++ app/main.py
@@ -46,6 +46,7 @@ def main() -> None:
     This function starts the Uvicorn server with the specified application instance,
     host, port, and reload settings.
     Parameters:
+        none
 
 
     None
